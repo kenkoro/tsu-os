07482ce91656e5c532b212f1eb9ee921c5f26443
kenkoro <frogharvard@gmail.com>
2024-11-17 16:35:25 +0700

    chore: init

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "tsu-os-1"
+
---
+++ src/main/java/com/kenkoro/Main.java
@@ -0,0 +1,43 @@
+package com.kenkoro;
+
+class Monitor {
+  private static boolean ready = false;
+
+  public synchronized void produce() {
+    if (ready) return;
+    try {
+      Thread.sleep(1000L);
+    } catch (InterruptedException e) {
+      System.out.println(e.getMessage());
+    }
+    ready = true;
+    System.out.println("Produced");
+    notify();
+  }
+
+  public synchronized void consume() {
+    try {
+      while (!ready) wait();
+    } catch (InterruptedException e) {
+      System.out.println(e.getMessage());
+    }
+    ready = false;
+    System.out.println("Consumed");
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    var monitor = new Monitor();
+    new Thread(() -> {
+      while (true) {
+        monitor.produce();
+      }
+    }).start();
+    new Thread(() -> {
+      while (true) {
+        monitor.consume();
+      }
+    }).start();
+  }
+}
